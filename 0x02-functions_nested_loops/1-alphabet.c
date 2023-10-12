43de883e275d240739eb7d1ea4c1e0ec391d657a
Williams Osimen <iamzhoga@tutamail.com>
2023-10-11 21:32:10 -0700

    prints lowercase alphabet in new line

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,15 +3,15 @@
 
 /**
  * print_aplhabet - Check description
- * Description : prints the alphabet, in lowercase with a new line
- * Return : Nothing
+ * Description: It prints the alphabet, in lowercase with a new line
+ * Return: Nothing
  */
 
 void print_alphabet(void)
 {
 	char i;
 
-	for (i='a'; i <='z'; i++)
+	for (i = 'a'; i <= 'z'; i++)
 		_putchar(i);
 	_putchar('\n');
 }
