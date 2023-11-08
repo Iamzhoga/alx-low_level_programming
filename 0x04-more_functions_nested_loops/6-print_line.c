98147505cf316a86bff9357802a2d8414b5af255
Williams Osimen <iamzhoga@tutamail.com>
2023-11-07 19:18:58 -0800

    print characters in n times

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,4 +1,20 @@
 #include "main.h"
 
 /**
+ * print numbers - Entry point to function
+ * @n: number of printing times
  *
+ * Return: Always zero
+ */
+
+void print_line(int n)
+{
+	int j;
+
+	for (j = 0; j < n; j++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+
+}
