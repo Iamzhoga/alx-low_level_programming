dbd70b67064c7b34f12d34069f52e86f88f9e35c
Williams Osimen <iamzhoga@tutamail.com>
2023-11-08 10:28:11 -0800

    removal of trailing whitespaces

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+/**
+ * print_diagonal - Draw a diagonal line of backslashes on the terminal.
+ * @n: variable inputation.
+ *
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+		_putchar('\n');
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
